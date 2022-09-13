411d87f2107c521a16bbe5a926412a470a3a5a1d
Rio <riobeggs1@gmail.com>
2022-09-13 21:44:49 +1200

    finsihed day 7

---
+++ 100_days_of_code/7_hangman.py
@@ -0,0 +1,384 @@
+import random
+
+# constants
+word_list = [
+    "abruptly",
+    "absurd",
+    "abyss",
+    "affix",
+    "askew",
+    "avenue",
+    "awkward",
+    "axiom",
+    "azure",
+    "bagpipes",
+    "bandwagon",
+    "banjo",
+    "bayou",
+    "beekeeper",
+    "bikini",
+    "blitz",
+    "blizzard",
+    "boggle",
+    "bookworm",
+    "boxcar",
+    "boxful",
+    "buckaroo",
+    "buffalo",
+    "buffoon",
+    "buxom",
+    "buzzard",
+    "buzzing",
+    "buzzwords",
+    "caliph",
+    "cobweb",
+    "cockiness",
+    "croquet",
+    "crypt",
+    "curacao",
+    "cycle",
+    "daiquiri",
+    "dirndl",
+    "disavow",
+    "dizzying",
+    "duplex",
+    "dwarves",
+    "embezzle",
+    "equip",
+    "espionage",
+    "euouae",
+    "exodus",
+    "faking",
+    "fishhook",
+    "fixable",
+    "fjord",
+    "flapjack",
+    "flopping",
+    "fluffiness",
+    "flyby",
+    "foxglove",
+    "frazzled",
+    "frizzled",
+    "fuchsia",
+    "funny",
+    "gabby",
+    "galaxy",
+    "galvanize",
+    "gazebo",
+    "giaour",
+    "gizmo",
+    "glowworm",
+    "glyph",
+    "gnarly",
+    "gnostic",
+    "gossip",
+    "grogginess",
+    "haiku",
+    "haphazard",
+    "hyphen",
+    "iatrogenic",
+    "icebox",
+    "injury",
+    "ivory",
+    "ivy",
+    "jackpot",
+    "jaundice",
+    "jawbreaker",
+    "jaywalk",
+    "jazziest",
+    "jazzy",
+    "jelly",
+    "jigsaw",
+    "jinx",
+    "jiujitsu",
+    "jockey",
+    "jogging",
+    "joking",
+    "jovial",
+    "joyful",
+    "juicy",
+    "jukebox",
+    "jumbo",
+    "kayak",
+    "kazoo",
+    "keyhole",
+    "khaki",
+    "kilobyte",
+    "kiosk",
+    "kitsch",
+    "kiwifruit",
+    "klutz",
+    "knapsack",
+    "larynx",
+    "lengths",
+    "lucky",
+    "luxury",
+    "lymph",
+    "marquis",
+    "matrix",
+    "megahertz",
+    "microwave",
+    "mnemonic",
+    "mystify",
+    "naphtha",
+    "nightclub",
+    "nowadays",
+    "numbskull",
+    "nymph",
+    "onyx",
+    "ovary",
+    "oxidize",
+    "oxygen",
+    "pajama",
+    "peekaboo",
+    "phlegm",
+    "pixel",
+    "pizazz",
+    "pneumonia",
+    "polka",
+    "pshaw",
+    "psyche",
+    "puppy",
+    "puzzling",
+    "quartz",
+    "queue",
+    "quips",
+    "quixotic",
+    "quiz",
+    "quizzes",
+    "quorum",
+    "razzmatazz",
+    "rhubarb",
+    "rhythm",
+    "rickshaw",
+    "schnapps",
+    "scratch",
+    "shiv",
+    "snazzy",
+    "sphinx",
+    "spritz",
+    "squawk",
+    "staff",
+    "strength",
+    "strengths",
+    "stretch",
+    "stronghold",
+    "stymied",
+    "subway",
+    "swivel",
+    "syndrome",
+    "thriftless",
+    "thumbscrew",
+    "topaz",
+    "transcript",
+    "transgress",
+    "transplant",
+    "triphthong",
+    "twelfth",
+    "twelfths",
+    "unknown",
+    "unworthy",
+    "unzip",
+    "uptown",
+    "vaporize",
+    "vixen",
+    "vodka",
+    "voodoo",
+    "vortex",
+    "voyeurism",
+    "walkway",
+    "waltz",
+    "wave",
+    "wavy",
+    "waxy",
+    "wellspring",
+    "wheezy",
+    "whiskey",
+    "whizzing",
+    "whomever",
+    "wimpy",
+    "witchcraft",
+    "wizard",
+    "woozy",
+    "wristwatch",
+    "wyvern",
+    "xylophone",
+    "yachtsman",
+    "yippee",
+    "yoked",
+    "youthful",
+    "yummy",
+    "zephyr",
+    "zigzag",
+    "zigzagging",
+    "zilch",
+    "zipper",
+    "zodiac",
+    "zombie",
+]
+logo = """ 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    """
+stages = [
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========""",
+    """
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+""",
+]
+
+
+def create_blanks(word_length: int) -> list:
+    display = []
+    for _ in range(word_length):
+        display += "_"
+
+    return display
+
+
+def guessed_already(guess: str, guessed_letters: list) -> bool:
+    if guess in guessed_letters:
+        return True
+
+    guessed_letters.append(guess)
+
+
+def incorrect_guess_check(guess: str, chosen_word: str) -> bool:
+    if guess not in chosen_word:
+        print(f"The letter '{guess}' is not in the word.")
+        return True
+
+
+def update_display(
+    word_length: int, chosen_word: str, display: list, guess: str
+) -> list:
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    return display
+
+
+def run(chosen_word: str) -> bool:
+    end_of_game = False
+    result = False
+    already_guessed = False
+    lives = 6
+    guessed_letters = []
+
+    word_length = len(chosen_word)
+    display = create_blanks(word_length)
+
+    while not end_of_game:
+        guess = input("Guess a letter: ").lower()
+
+        # Check is letter has already been guessed
+        already_guessed = guessed_already(guess, guessed_letters)
+        if already_guessed:
+            print(f"You have already guessed the letter '{guess}'.")
+            continue
+
+        # Check if guess is incorrect
+        guess_is_incorrect = incorrect_guess_check(guess, chosen_word)
+        if guess_is_incorrect:
+            lives -= 1
+            if lives == 0:
+                end_of_game = True
+
+        # Updates and prints the display
+        updated_display = update_display(word_length, chosen_word, display, guess)
+        print(f"{' '.join(display)}")
+
+        # Check if user has got all letters.
+        if "_" not in updated_display:
+            result = True
+            end_of_game = True
+
+        print(stages[lives])
+
+    return result
+
+
+def main() -> None:
+    print(logo)
+
+    chosen_word = random.choice(word_list)
+
+    # Testing code
+    # print(f"Solution: {chosen_word}.")
+
+    game_won = run(chosen_word)
+
+    if game_won:
+        print("You win.")
+
+    if not game_won:
+        print("You lose.")
+        print(f"The word was {chosen_word}.")
+
+
+if __name__ == "__main__":
+    main()
