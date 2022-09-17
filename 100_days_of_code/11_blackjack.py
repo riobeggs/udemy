e3ce8452c04f3e5c31f8acd48a62cc68e47f8f41
Rio <riobeggs1@gmail.com>
2022-09-17 19:19:59 +1200

    working on hit function, calc card total function and winner function.

--- 100_days_of_code/11_blackjack.py
+++ 100_days_of_code/11_blackjack.py
@@ -1,10 +1,16 @@
 import random
+import sys
 
 
 def intro() -> str:
     """
-    Blackjack ascii art.
+    Blackjack ascii art and game start.
     """
+
+    confirmation = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+
+    if confirmation != "y":
+        sys.exit()
 
     logo = """
     .------.            _     _            _    _            _    
@@ -28,18 +34,79 @@ def draw_card() -> int:
     return distributed_card
 
 
-def user() -> None:
-    pass
+def hit() -> bool:
+    selection_is_valid = False
+
+    while not selection_is_valid:
+        selection = input("Type 'y' to get another card, type 'n' to pass: ")
+        if selection == "y" or selection == "n":
+            break
+
+    if selection == "y":
+        return True
+
+    if selection == "n":
+        return False
 
 
-def computer() -> None:
-    pass
+def calculate_cards(user, computer) -> list:
+    total = []
+
+    user = int(sum(user))
+    computer = int(sum(computer))
+
+    return total
+
+
+def determine_winner(calculate_cards: list) -> str:
+    user = calculate_cards[0]
+    computer = calculate_cards[1]
+
+    if user > 21 and computer <= 21:
+        return "lose."
+
+    if user <= 21 and computer > 21:
+        return "win." 
+
+    if user <= 21 and computer <= 21:
+        if user == computer:
+            return "drew."
+
+
+def user() -> list:
+    card_list = []
+    play = True
+
+    for i in range(0, 2):
+        card = draw_card()
+        card_list.append(card)
+
+    print(f"Your cards: {card_list}")
+
+    while play:
+        take_card = hit()
+        if not take_card:
+            break
+
+        card = draw_card()
+        card_list.append(card)
+
+    return card_list
+
+
+def computer() -> list:
+    card_list = []
+    card = draw_card()
+    card_list.append(card)
+
+    return card_list
 
 
 def main() -> None:
     print(intro())
 
-    card = draw_card()
+    run = user()
+    print(run)    
 
 
 if __name__ == "__main__":
