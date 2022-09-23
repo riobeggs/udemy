240c0978eb8021c4fb35709d5f6dc5051c1ba856
Rio <riobeggs1@gmail.com>
2022-09-23 14:05:37 +1200

    finished number guessing game.

--- 100_days_of_code/12_number_guessing_game.py
+++ 100_days_of_code/12_number_guessing_game.py
@@ -1,15 +1,11 @@
-# Number Guessing Game Objectives:
-
-# Include an ASCII art logo.
-# Allow the player to submit a guess for a number between 1 and 100.
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer.
-# If they got the answer correct, show the actual answer to the player.
-# Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player.
-# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+import random
 
 
 def intro() -> str:
+    """
+    Prints game logo and provides game information.
+    """
+
     logo = """
   ____                       _   _            _   _                 _               
  / ___|_   _  ___  ___ ___  | |_| |__   ___  | \ | |_   _ _ __ ___ | |__   ___ _ __ 
@@ -20,10 +16,17 @@ def intro() -> str:
 
     return f"""{logo}
 Welcome to the number guessing game!
-I am thinking of a number between 1 and 100."""
+I am thinking of a number between 1 and 100.
+"""
 
 
 def difficulty() -> int:
+    """
+    Asks the user to choose the difficulty.
+
+    returns an integer relative to the amount of guesses they get for the chosen difficulty.
+    """
+
     difficulty_options = {"easy": 10, "hard": 5}
     chosen_difficulty = None
 
@@ -37,9 +40,71 @@ def difficulty() -> int:
     return chosen_difficulty
 
 
+def chosen_number() -> int:
+    """
+    A random number between 1 and 100 is selected.
+
+    returns number as an integer.
+    """
+
+    number = random.randrange(1, 101)
+
+    return number
+
+
+def guess(guesses_left: int, answer: int) -> int:
+    """
+    Takes the users guess as an input.
+    Lets the user know how many guesses they have left and hints relative to the answer.
+
+    returns the users guess as an integer.
+    """
+
+    for guess_number in range(guesses_left):
+        valid_guess = False
+        print(
+            f"\nYou have {guesses_left - guess_number} attempts remaining to guess the number."
+        )
+
+        while not valid_guess:
+            guess = input("Make a guess: ")
+            if int(guess):
+                guess = int(guess)
+                valid_guess = True
+
+        if guess < answer:
+            print("Too low.")
+
+        if guess > answer:
+            print("Too high.")
+
+        if guess == answer:
+            return guess
+
+    return guess
+
+
+def results(users_guess: bool, answer: int) -> str:
+    """
+    returns a string that lets user know if they guessed the correct number or not.
+    """
+
+    computers_number = f"The answer was {answer}."
+
+    if users_guess == answer:
+        return "You got it! " + computers_number
+
+    return "You've run out of guesses, you lose. " + computers_number
+
+
 def main() -> None:
     print(intro())
-    chosen_difficulty = difficulty()
+
+    guesses_left = difficulty()
+    answer = chosen_number()
+    users_guess = guess(guesses_left, answer)
+
+    print(results(users_guess, answer))
 
 
 if __name__ == "__main__":
