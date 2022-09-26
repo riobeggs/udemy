b1308ca647018eb40f72c3b8e62cee5a19831be9
Rio <riobeggs1@gmail.com>
2022-09-26 21:33:23 +1300

    progress in coffee machine program

--- 100_days_of_code/15_coffee_machine.py
+++ 100_days_of_code/15_coffee_machine.py
@@ -1,3 +1,5 @@
+import sys
+
 # Constants
 MENU = {
     "espresso": {
@@ -25,8 +27,82 @@ MENU = {
     },
 }
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
+
+def resources_report(resources: dict, money: int = 0) -> str:
+    available_resources = f"""
+Water: {resources["water"]}ml,
+Milk: {resources["milk"]}ml,
+Coffee: {resources["coffee"]}g,
+Money: ${money}
+    """
+
+    return available_resources
+
+
+def resources_sufficient(chosen_coffee: str, available_resources: dict) -> bool:
+    chosen_coffee_resources = MENU[chosen_coffee["ingredients"]]
+    error = 0
+
+    if chosen_coffee_resources["water"] > available_resources["water"]:
+        print("Sorry there is not enough water.")
+        error += 1
+
+    if chosen_coffee_resources["milk"] > available_resources["milk"]:
+        print("Sorry there is not enough milk.")
+        error += 1
+
+    if chosen_coffee_resources["coffee"] > available_resources["coffee"]:
+        print("Sorry there is not enough coffee.")
+        error += 1
+
+    return error == 0
+
+
+def make_coffee(chosen_coffee: str, available_resources: dict) -> str:
+    if chosen_coffee == "espresso":
+        if resources_sufficient(chosen_coffee, available_resources):
+            pass
+
+    if chosen_coffee == "latte":
+        if resources_sufficient(chosen_coffee, available_resources):
+            pass
+
+    if chosen_coffee == "cappuccino":
+        if resources_sufficient(chosen_coffee, available_resources):
+            pass
+
+
+def get_coffee_type(available_resources: dict) -> str:
+    coffee_options = ["espresso", "latte", "cappuccino", "report", "off"]
+    chosen_coffee = None
+
+    while chosen_coffee not in coffee_options:
+        chosen_coffee = input(
+            "What would you like? (espresso/latte/cappuccino): "
+        ).lower()
+
+    if chosen_coffee == "report":
+        print(resources_report(available_resources))
+
+    if chosen_coffee == "off":
+        print("Coffee machine turning off...")
+        sys.exit()
+
+    return chosen_coffee
+
+
+def main() -> None:
+    coffee_machine_operating = True
+    available_resources = {
+        "water": 300,
+        "milk": 200,
+        "coffee": 100,
+    }
+
+    while coffee_machine_operating:
+        chosen_coffee = get_coffee_type(available_resources)
+        make_coffee(chosen_coffee, available_resources)
+
+
+if __name__ == "__main__":
+    main()
