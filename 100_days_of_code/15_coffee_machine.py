ecbe81785d3b22e1e37fa4312d1cec8f74f1c6b0
Rio <riobeggs1@gmail.com>
2022-09-27 23:04:42 +1300

    finished coffee machine project

--- 100_days_of_code/15_coffee_machine.py
+++ 100_days_of_code/15_coffee_machine.py
@@ -5,9 +5,10 @@ MENU = {
     "espresso": {
         "ingredients": {
             "water": 50,
+            "milk": 0,
             "coffee": 18,
         },
-        "cost": 1.5,
+        "cost": 3.5,
     },
     "latte": {
         "ingredients": {
@@ -15,7 +16,7 @@ MENU = {
             "milk": 150,
             "coffee": 24,
         },
-        "cost": 2.5,
+        "cost": 5.5,
     },
     "cappuccino": {
         "ingredients": {
@@ -23,85 +24,150 @@ MENU = {
             "milk": 100,
             "coffee": 24,
         },
-        "cost": 3.0,
+        "cost": 6,
     },
 }
+COFFEE_LOGO = """
+                       .
+                        `:.
+                          `:.
+                  .:'     ,::
+                 .:'      ;:'
+                 ::      ;:'
+                  :    .:'
+                   `.  :.
+          _________________________
+         : _ _ _ _ _ _ _ _ _ _ _ _ :
+     ,---:".".".".".".".".".".".".":
+    : ,'"`::.:.:.:.:.:.:.:.:.:.:.::'
+    `.`.  `:-===-===-===-===-===-:'
+      `.`-._:                   :
+        `-.__`.               ,' 
+    ,--------`"`-------------'--------.
+     `"--.__                   __.--"'
+            `""-------------""'
+"""
 
 
-def resources_report(resources: dict, money: int = 0) -> str:
+def resources_report(resources: dict) -> str:
+    """
+    Returns a string of the resources available in order to make a coffee.
+    """
     available_resources = f"""
 Water: {resources["water"]}ml,
 Milk: {resources["milk"]}ml,
 Coffee: {resources["coffee"]}g,
-Money: ${money}
+Money: ${resources["money"]}
     """
 
     return available_resources
 
 
-def resources_sufficient(chosen_coffee: str, available_resources: dict) -> bool:
-    chosen_coffee_resources = MENU[chosen_coffee["ingredients"]]
-    error = 0
-
-    if chosen_coffee_resources["water"] > available_resources["water"]:
-        print("Sorry there is not enough water.")
-        error += 1
-
-    if chosen_coffee_resources["milk"] > available_resources["milk"]:
-        print("Sorry there is not enough milk.")
-        error += 1
-
-    if chosen_coffee_resources["coffee"] > available_resources["coffee"]:
-        print("Sorry there is not enough coffee.")
-        error += 1
-
-    return error == 0
-
-
-def make_coffee(chosen_coffee: str, available_resources: dict) -> str:
-    if chosen_coffee == "espresso":
-        if resources_sufficient(chosen_coffee, available_resources):
-            pass
-
-    if chosen_coffee == "latte":
-        if resources_sufficient(chosen_coffee, available_resources):
-            pass
-
-    if chosen_coffee == "cappuccino":
-        if resources_sufficient(chosen_coffee, available_resources):
-            pass
-
-
 def get_coffee_type(available_resources: dict) -> str:
-    coffee_options = ["espresso", "latte", "cappuccino", "report", "off"]
+    """
+    Returns the coffee type specified by the user as a string.
+    
+    If the user inputs off, the program quits.
+    If the user inputs report, resources_report is executed.
+    """
+    coffee_options = ["espresso", "latte", "cappuccino", "off"]
     chosen_coffee = None
+
+    print("Welcome to the Coffee machine!")
 
     while chosen_coffee not in coffee_options:
         chosen_coffee = input(
-            "What would you like? (espresso/latte/cappuccino): "
+            "\nWhat would you like? (espresso/latte/cappuccino): "
         ).lower()
 
-    if chosen_coffee == "report":
-        print(resources_report(available_resources))
+        if chosen_coffee == "report":
+            print(resources_report(available_resources))
 
     if chosen_coffee == "off":
-        print("Coffee machine turning off...")
+        print("\nCoffee machine turning off...\n")
         sys.exit()
 
     return chosen_coffee
 
 
+def processed_resources(chosen_coffee: str, available_resources: dict) -> bool:
+    """
+    Returns True if there are enough resources left to make the users coffee.
+    """
+    chosen_coffee_ingredients = MENU[chosen_coffee]["ingredients"]
+
+    ingredients_set = ("water", "milk", "coffee")
+    error = 0
+
+    for ingredient in ingredients_set:
+        if (
+            chosen_coffee_ingredients[f"{ingredient}"]
+            > available_resources[f"{ingredient}"]
+        ):
+            print(f"Sorry there is not enough {ingredient}.")
+            error += 1
+        else:
+            available_resources[f"{ingredient}"] -= chosen_coffee_ingredients[
+                f"{ingredient}"
+            ]
+
+    return error == 0
+
+
+def processed_coins(chosen_coffee: str, available_resources: dict) -> bool:
+    """
+    Returns True if the user has deposited enough money for their coffee.
+    """
+    chosen_coffee_cost = MENU[chosen_coffee]["cost"]
+    invalid_coins = True
+
+    while invalid_coins:
+        inserted_coins = input(
+            f"{chosen_coffee} costs ${chosen_coffee_cost}.\nInsert coins: $"
+        )
+        try:
+            inserted_coins = float(inserted_coins)
+            invalid_coins = False
+        except:
+            pass
+
+    if inserted_coins == chosen_coffee_cost:
+        available_resources["money"] += inserted_coins
+        print("\nThank you.")
+        return True
+
+    if inserted_coins > chosen_coffee_cost:
+        available_resources["money"] += inserted_coins
+        change = inserted_coins - chosen_coffee_cost
+        print(f"Thank you.\nYou recieve ${change} in change.")
+        return True
+
+    print("\nSorry that's not enough money. Money refunded.")
+    return False
+
+
+def make_coffee(chosen_coffee: str, available_resources: dict) -> str:
+    """
+    Executes coin and resource processing.
+
+    Returns the users coffee as a string if successful.
+    """
+    if processed_coins(chosen_coffee, available_resources):
+        if processed_resources(chosen_coffee, available_resources):
+            return f"\nHere is your {chosen_coffee}. Enjoy!\n☕\n\n"
+
+    return "Please try again.\n\n"
+
+
 def main() -> None:
     coffee_machine_operating = True
-    available_resources = {
-        "water": 300,
-        "milk": 200,
-        "coffee": 100,
-    }
+    available_resources = {"water": 300, "milk": 200, "coffee": 100, "money": 20}
+
+    print(COFFEE_LOGO)
 
     while coffee_machine_operating:
         chosen_coffee = get_coffee_type(available_resources)
-        make_coffee(chosen_coffee, available_resources)
+        print(make_coffee(chosen_coffee, available_resources))
 
 
 if __name__ == "__main__":
