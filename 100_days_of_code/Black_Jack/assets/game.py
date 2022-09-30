6f3b59eed3eed9a31c9e76de5b677dd0019fb53c
The Sam <muer.powell@gmail.com>
2022-10-01 10:51:02 +1300

    Upload a class skeleton for blackjack

---
+++ 100_days_of_code/Black_Jack/assets/game.py
@@ -0,0 +1,74 @@
+from typing import Iterable, List
+
+from .player import Player
+from .errors import MethodNotImplemented
+
+
+class Game:
+    """The black jack game engine."""
+
+    _players: List[Player] = []
+
+    def __init__(self, *args):
+        """Things to initialize when making a game class"""
+
+        for item in args:
+            if isinstance(item, Player):
+                self.add_player(item)
+                
+
+    def add_player(self, player: Player) -> bool:
+        """Add a player to the game. """
+        assert isinstance(player, Player)
+
+    def add_players(self, players: Iterable[Player]) -> bool:
+        """Add multiple players to a game at once."""
+        success = False
+        
+        assert isinstance(players, Iterable)
+
+        # add_player call this
+
+        return success
+
+    def deal_cards(self) -> None:
+        """Deal cards to each player in the game."""
+        raise MethodNotImplemented()
+
+    def deal_card(self, player: Player) -> None:
+        """Deal a card to a single player."""
+        raise MethodNotImplemented()
+
+    def print_game_state(self) -> None:
+        """Prints the current game state of each player."""
+        raise MethodNotImplemented()
+
+    @staticmethod
+    def introduction():
+        """Print information about the Black Jack game"""
+        raise MethodNotImplemented()
+
+    def play(self):
+        """Play a game of Black Jack"""
+        play = True
+        while play:
+            raise MethodNotImplemented()
+
+    def restart(self):
+        """Reset the game of Black Jack"""
+        raise MethodNotImplemented()
+
+    def _determine_winner(self) -> Player:
+        """
+        Internal method to determine who won the game.
+        
+        Returns the winning player.
+        """
+        raise MethodNotImplemented()
+
+    def print_game_end_information(self) -> None:
+        """
+        Prints the winner of the game to the console.
+        Might also print the sate of the game to the console.
+        """
+        raise MethodNotImplemented()
